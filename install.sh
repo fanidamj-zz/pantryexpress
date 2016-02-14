50b5cc119e222fbeffda52505a2aafe9cb75a57a
Justin Pirtle <justonian@noreply.github.com>
2016-02-13 17:03:44 -0800

    Add common installation

---
+++ install.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+# Die if any subcommand fails
+set -x
+if [ $PWD = "/home/ubuntu/workspace" ]; then
+  rm -rf ~/workspace
+  mkdir -p ~/workspace
+fi
+set -e
+echo -e "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g npm
+sudo npm install -g serverless@0.0.15 
+sudo npm install -g yo generator-angular@0.15.0 generator-karma
+yes | bower
+sudo pip install awscli
+echo "Setting up pantryexpress-api"
+yes | git clone git@github.com:justonian/pantryexpress-api
+echo "Install backend dependencies"
+cd pantryexpress-api
+npm install
+echo "Setting up pantryexpress-web"
+cd ..
+yes | git clone git@github.com:justonian/pantryexpress-web
+cd pantryexpress-web
+npm install
+bower install
+grunt
