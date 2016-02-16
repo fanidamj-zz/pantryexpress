7844b1e25281f49d60e1580453446f363fb76d39
Justin Pirtle <justonian@users.noreply.github.com>
2016-02-16 15:46:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ if [ $PWD = "/home/ubuntu/workspace" ]; then
   rm -rf {,.[!.],..?}*
 fi
 echo -e "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-sudo nvm use stable
-sudo npm install -g grunt-cli
-sudo npm install -g serverless@0.0.15 
-sudo npm install -g yo generator-angular@0.15.0 generator-karma
+touch ~/.bash_profile
+echo "#/bin/bash" >> ~/.bash_profile
+echo "export NVM_DIR="$HOME/.nvm"" >> ~/.bash_profile
+echo "[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"" >> ~/.bash_profile
+sh ~/.bash_profile
+nvm use stable
+npm install -g grunt-cli
+npm install -g serverless@0.0.15 
+npm install -g yo generator-angular@0.15.0 generator-karma
 yes | bower
 sudo pip install awscli
 echo "Setting up pantryexpress-api"
