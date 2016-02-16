1867024446c51f19307bbd4ce1268578ef8bb3b0
Justin Pirtle <justonian@users.noreply.github.com>
2016-02-16 15:19:08 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ echo -e "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm install -g npm
+sudo npm install -g grunt-cli
 sudo npm install -g serverless@0.0.15 
 sudo npm install -g yo generator-angular@0.15.0 generator-karma
 yes | bower
