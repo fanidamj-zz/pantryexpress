d1c939616bb65c0bc5309762630d99e7662232d0
Justin Pirtle <justonian@users.noreply.github.com>
2016-02-16 15:23:07 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ if [ $PWD = "/home/ubuntu/workspace" ]; then
   rm -rf {,.[!.],..?}*
 fi
 echo -e "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g npm
+nvm use stable
 sudo npm install -g grunt-cli
 sudo npm install -g serverless@0.0.15 
 sudo npm install -g yo generator-angular@0.15.0 generator-karma
