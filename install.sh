e834ad9de28cbd99371c34213caf998f545878cf
Justin Pirtle <justonian@users.noreply.github.com>
2016-02-16 16:02:45 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,6 @@ if [ $PWD = "/home/ubuntu/workspace" ]; then
   rm -rf {,.[!.],..?}*
 fi
 echo -e "\nHost github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-touch ~/.bash_profile
-echo "#/bin/bash" >> ~/.bash_profile
-echo "export NVM_DIR="$HOME/.nvm"" >> ~/.bash_profile
-echo "[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"" >> ~/.bash_profile
-sh ~/.bash_profile
-sh ~/.nvm/nvm.sh use stable
 npm install -g grunt-cli
 npm install -g serverless@0.0.15 
 npm install -g yo generator-angular@0.15.0 generator-karma
